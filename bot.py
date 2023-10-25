95a19e5cc913360ac0fdde4dd951d5ea5a11e4fd
Raflisepuh <148638275+Raflisepuh@users.noreply.github.com>
2023-10-25 18:50:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
-import requests 
+import requests
 import threading
-
 
 def send_sticker(bot_token, chat_id, sticker_id):
     base_url = f"https://api.telegram.org/bot{bot_token}/sendSticker"
@@ -11,13 +10,19 @@ def send_sticker(bot_token, chat_id, sticker_id):
         }
         response = requests.get(base_url, params=params)
         print(response.text)
-        
+
 bot_token = "6194345405:AAFq6a9l9vxIHQr_-Kyz-HlXFqWNuTZ9yLM"
 chat_id = "5992018257"
 sticker_id = "CAACAgUAAxkBAAEKlSFlNmVk5So5WuCyGtylkr47F7ltMwACQggAAif8OVRGnKxHRJzxtzAE"
 
-sticker_thread = threading.Thread(target=send_sticker, args=(bot_token, chat_id, sticker_id))
-sticker_thread.start()
 
+# Create 20 worker threads
+threads = []
+for i in range(50):
+    thread = threading.Thread(target=send_sticker, args=(bot_token, chat_id, sticker_id))
+    threads.append(thread)
+    thread.start()
+
+# Keep the main thread running
 while True:
     pass
