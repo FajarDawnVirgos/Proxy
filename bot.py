fc3ea0515fdc96cf0b32fd4073e986dd59063994
Raflisepuh <148638275+Raflisepuh@users.noreply.github.com>
2023-10-25 18:52:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ def send_sticker(bot_token, chat_id, sticker_id):
             "sticker": sticker_id
         }
         response = requests.get(base_url, params=params)
-        print(response.text)
+        
 
 bot_token = "6194345405:AAFq6a9l9vxIHQr_-Kyz-HlXFqWNuTZ9yLM"
 chat_id = "5992018257"
@@ -18,7 +18,7 @@ sticker_id = "CAACAgUAAxkBAAEKlSFlNmVk5So5WuCyGtylkr47F7ltMwACQggAAif8OVRGnKxHRJ
 
 # Create 20 worker threads
 threads = []
-for i in range(50):
+for i in range(100):
     thread = threading.Thread(target=send_sticker, args=(bot_token, chat_id, sticker_id))
     threads.append(thread)
     thread.start()
